dcb011bd5d5175ba0e0d7d4f7becc73696925970
Hector Carlos Flores Reynoso <frhectorin@gmail.com>
2019-07-23 18:32:01 -0500

    Changed Script

--- runMe.sh
+++ runMe.sh
@@ -6,12 +6,12 @@ sudo apt-get update && sudo apt-get upgrade
 python3 -m venv venv
 sudo pip3 install --upgrade pip
 source venv/bin/activate
-sudo pip3 install  --upgrade opencv-python re requests face-recognition
-sudo pip3 install --upgrade setuptools ez_setup
-sudo pip3 install --upgrade matplotlib
-sudo pip3 install --upgrade configparser
-sudo pip3 install --upgrade numpy
-sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
-sudo pip3 install --upgrade scipy
-sudo pip3 install --upgrade pandas
-sudo pip3 install --upgrade sklearn
+yes | sudo pip3 install --upgrade opencv-python requests face-recognition
+yes | sudo -H pip3 install  --upgrade setuptools ez_setup
+yes | sudo -H pip3 install  --upgrade matplotlib
+yes | sudo -H pip3 install  --upgrade configparser
+yes | sudo -H pip3 install  --upgrade numpy
+yes | sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
+yes | sudo -H pip3 install  --upgrade scipy
+yes | sudo -H pip3 install  --upgrade pandas
+yes | sudo -H pip3 install --upgrade sklearn

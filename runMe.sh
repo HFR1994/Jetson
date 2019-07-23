49238459a2a6dcf0bbfdea6437ad9ba909427850
Hector Carlos Flores Reynoso <frhectorin@gmail.com>
2019-07-23 18:07:18 -0500

    Changed LF

--- runMe.sh
+++ runMe.sh
@@ -6,12 +6,12 @@ sudo apt-get update && sudo apt-get upgrade
 python3 -m venv venv
 sudo pip3 install --upgrade pip
 source venv/bin/activate
-sudo pip3 install  --upgrade pickle opencv-python
-sudo pip3.5 install --upgrade setuptools ez_setup
-sudo pip3.5 install --upgrade matplotlib
-sudo pip3.5 install --upgrade configparser
-sudo pip3.5 install --upgrade numpy
+sudo pip3 install  --upgrade opencv-python re requests face-recognition
+sudo pip3 install --upgrade setuptools ez_setup
+sudo pip3 install --upgrade matplotlib
+sudo pip3 install --upgrade configparser
+sudo pip3 install --upgrade numpy
 sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
-sudo pip3.5 install --upgrade scipy
-sudo pip3.5 install --upgrade pandas
-sudo pip3.5 install --upgrade sklearn
+sudo pip3 install --upgrade scipy
+sudo pip3 install --upgrade pandas
+sudo pip3 install --upgrade sklearn

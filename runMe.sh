4b3ebe9c4e14edc07a67f0b3bcc54ca251d5bdf7
Hector Flores <frhectorin@gmail.com>
2019-07-24 00:35:07 -0500

    Changed runMe.sh

--- runMe.sh
+++ runMe.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-sudo apt-get update && sudo apt-get upgrade
+yes |sudo apt-get update && sudo apt-get upgrade
+yes | sudo apt-get install python3-venv
 python3 -m venv venv
-sudo pip3 install --upgrade pip
+yes | sudo pip3 install --upgrade pip
 source venv/bin/activate
-yes | sudo pip3 install --upgrade opencv-python requests face-recognition
+yes | sudo -H pip3 install --upgrade opencv-python requests face-recognition
 yes | sudo -H pip3 install  --upgrade setuptools ez_setup
 yes | sudo -H pip3 install  --upgrade matplotlib
 yes | sudo -H pip3 install  --upgrade configparser
